2d576b6abe03e596e36db3375e072ace0e1e0dfb
mrUnknown2904 <gokulnathgnanasekeran7@gmail.com>
2024-05-24 19:04:32 +0530

    configured backend

--- main/version.tf
+++ main/version.tf
@@ -5,5 +5,11 @@ terraform {
       version = "~> 5.51.0"
     }
   }
+  backend "s3" {
+    bucket = "aws-654654413093-statefile"
+    key = "nonprod/nonprod.tfstate"
+    region = "ap-south-1"
+    encrypt = true
+  }
   required_version = "> 1.8.0"
 }
